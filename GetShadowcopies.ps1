1cd844a43d6bb9bfa8294bcdd9b809b59415fb0f
rebuildtech <69626481+rebuildtech@users.noreply.github.com>
2021-07-28 16:17:00 +0530

    Update GetShadowcopies.ps1
    Updated the script to run for Remote machines as well

--- GetShadowcopies.ps1
+++ GetShadowcopies.ps1
@@ -1,6 +1,11 @@
   #script to get the details of Shadow copy.
   
 
+  $servers = gc -Path C:\path\serverlist.txt
+
+  foreach ($s in $servers)
+  Invoke-Command -ComputerName $s -ScriptBlock {
+
   #get the drive letter 
     $volumes = @{}
     $allvs = Get-WmiObject win32_volume
@@ -16,4 +21,6 @@
     Get-WmiObject Win32_shadowcopy  | Select OriginatingMachine, @{n="DriveID" ; E= {$volume.Values} } , VolumeName , Count 
     } 
 
-    Export-csv -Path C:\Users\test\output.csv
+    }
+
+    Export-csv -Path C:\Users\test\output.csv -Delimiter ';' -Append
